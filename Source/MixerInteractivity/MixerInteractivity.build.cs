672928e8ac3114df11798e3fecea892b4d96ba21
James Yarrow <jamesya@microsoft.com>
2017-07-07 13:27:09 -0700

    Adding pre-built libs for interactive-cpp SDK to simplify use in UE

--- Source/MixerInteractivity/MixerInteractivity.build.cs
+++ Source/MixerInteractivity/MixerInteractivity.build.cs
@@ -17,10 +17,12 @@ public class MixerInteractivity : ModuleRules
 				"Json",
 			});
 
-		string MixerSdkFolder = Path.Combine(ModuleDirectory, "..", "..", "ThirdParty", "interactive-cpp");
+		string ThirdPartyFolder = Path.Combine(ModuleDirectory, "..", "..", "ThirdParty");
+		string MixerSdkFolder = Path.Combine(ThirdPartyFolder, "interactive-cpp");
 		PrivateIncludePaths.Add(Path.Combine(MixerSdkFolder, "Include"));
 		PrivateIncludePaths.Add(Path.Combine(MixerSdkFolder, "cpprestsdk", "Release", "include"));
 
+		PublicLibraryPaths.Add(Path.Combine(ThirdPartyFolder, "Lib", Target.Platform.ToString()));
 
 		if (Target.Platform == UnrealTargetPlatform.Win64 || Target.Platform == UnrealTargetPlatform.Win32)
 		{
@@ -31,37 +33,17 @@ public class MixerInteractivity : ModuleRules
 					"WebBrowser",
 					"SlateCore",
 					"Slate",
-					"zlib"
 				});
-
-			string MsArchFragment = Target.Platform == UnrealTargetPlatform.Win64 ? "x64" : "Win32";
-			string BoostArchFragment = Target.Platform == UnrealTargetPlatform.Win64 ? "address-model-64" : "address-model-32";
-			string OpenSslArchFragment = Target.Platform == UnrealTargetPlatform.Win64 ? "x64" : "x86";
-
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "Binaries", "Release", MsArchFragment, "Interactivity.Win32.Cpp"));
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "Binaries", "Release", MsArchFragment, "Casablanca"));
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "External", "Packages", "boost_system-vc140.1.58.0-vs140rc", "lib", "native", BoostArchFragment, "lib"));
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "External", "Packages", "boost_date_time-vc140.1.58.0-vs140rc", "lib", "native", BoostArchFragment, "lib"));
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "External", "Packages", "boost_regex-vc140.1.58.0-vs140rc", "lib", "native", BoostArchFragment, "lib"));
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "External", "Packages", string.Format("openssl.v140.windesktop.msvcstl.static.rt-dyn.{0}.1.0.2.0", OpenSslArchFragment), "lib", "native", "v140", "windesktop", "msvcstl", "static", "rt-dyn", OpenSslArchFragment, "release"));
 
 			PublicAdditionalLibraries.Add("Interactivity.Win32.Cpp.lib");
 			PublicAdditionalLibraries.Add("cpprest140_2_9.lib");
 			PublicAdditionalLibraries.Add("winhttp.lib");
 			PublicAdditionalLibraries.Add("crypt32.lib");
 			PublicAdditionalLibraries.Add("bcrypt.lib");
-
-			// Standard UE version of OpenSSL unfortunately won't do here.
-			PublicAdditionalLibraries.Add("libeay32.lib");
-			PublicAdditionalLibraries.Add("ssleay32.lib");
 		}
 		else if (Target.Platform == UnrealTargetPlatform.XboxOne)
 		{
 			Definitions.Add("PLATFORM_SUPPORTS_MIXER_OAUTH=0");
-
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "Binaries", "Release", "Durango", "casablanca140.Xbox"));
-			PublicLibraryPaths.Add(Path.Combine(MixerSdkFolder, "Binaries", "Release", "Durango", "Interactivity.Xbox.Cpp"));
-
 
 			PublicAdditionalLibraries.Add("Interactivity.Xbox.Cpp.lib");
 			PublicAdditionalLibraries.Add("casablanca140.xbox.lib");
--- ThirdParty/BuildThirdPartyLibraries.bat
+++ ThirdParty/BuildThirdPartyLibraries.bat
@@ -15,3 +15,10 @@ msbuild interactive-cpp\cpprestsdk\release\src\build\vs14.uwp\cpprestsdk140.uwp.
 
 msbuild %~dp0\interactive-cpp\Build\Interactivity.UWP.Cpp\Interactivity.UWP.Cpp.vcxproj /p:Configuration=Release /p:Platform=x64
 msbuild %~dp0\interactive-cpp\Build\Interactivity.UWP.Cpp\Interactivity.UWP.Cpp.vcxproj /p:Configuration=Release /p:Platform=Win32
+
+xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\Durango\casablanca140.xbox\*.* %~dp0\Lib\XboxOne
+xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\Durango\Interactivity.Xbox.Cpp\*.* %~dp0\Lib\XboxOne
+xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\Win32\Casablanca\*.* %~dp0\Lib\Win32
+xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\Win32\Interactivity.Win32.Cpp\*.* %~dp0\Lib\Win32
+xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\x64\Casablanca\*.* %~dp0\Lib\Win64
+xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\x64\Interactivity.Win32.Cpp\*.* %~dp0\Lib\Win64
