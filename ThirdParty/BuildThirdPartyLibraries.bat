cdfc4cb47962290ae0f9108bc67d59603a29b69a
James Yarrow <jamesya@microsoft.com>
2017-07-18 10:21:22 -0700

    Build script - remove unnecessary platform, fix error in path.

--- ThirdParty/BuildThirdPartyLibraries.bat
+++ ThirdParty/BuildThirdPartyLibraries.bat
@@ -1,7 +1,7 @@
 %~dp0\nuget restore %~dp0\interactive-cpp\cpprestsdk\release\src\build\vs14.static\packages.config -PackagesDirectory %~dp0\interactive-cpp\Tools\Packages
 
 msbuild %~dp0\interactive-cpp\cpprestsdk\release\src\build\vs14.static\casablanca140.static.vcxproj /p:Configuration=Release /p:Platform=x64 
-msbuild interactive-cpp\cpprestsdk\release\src\build\vs14.static\casablanca140.static.vcxproj /p:Configuration=Release /p:Platform=Win32
+msbuild %~dp0\interactive-cpp\cpprestsdk\release\src\build\vs14.static\casablanca140.static.vcxproj /p:Configuration=Release /p:Platform=Win32
 
 msbuild %~dp0\interactive-cpp\Build\Interactivity.Win32.Cpp\Interactivity.Win32.Cpp.vcxproj /p:Configuration=Release /p:Platform=x64
 msbuild %~dp0\interactive-cpp\Build\Interactivity.Win32.Cpp\Interactivity.Win32.Cpp.vcxproj /p:Configuration=Release /p:Platform=Win32
@@ -9,12 +9,6 @@ msbuild %~dp0\interactive-cpp\Build\Interactivity.Win32.Cpp\Interactivity.Win32.
 msbuild %~dp0\interactive-cpp\cpprestsdk\release\src\build\vs14.xbox\casablanca140.xbox.vcxproj /p:Configuration=Release /p:Platform=Durango
 
 msbuild %~dp0\interactive-cpp\Build\Interactivity.Xbox.Cpp\Interactivity.Xbox.Cpp.vcxproj /p:Configuration=Release /p:Platform=Durango
-
-msbuild %~dp0\interactive-cpp\cpprestsdk\release\src\build\vs14.uwp\cpprestsdk140.uwp.static.vcxproj /p:Configuration=Release /p:Platform=x64
-msbuild interactive-cpp\cpprestsdk\release\src\build\vs14.uwp\cpprestsdk140.uwp.static.vcxproj /p:Configuration=Release /p:Platform=Win32
-
-msbuild %~dp0\interactive-cpp\Build\Interactivity.UWP.Cpp\Interactivity.UWP.Cpp.vcxproj /p:Configuration=Release /p:Platform=x64
-msbuild %~dp0\interactive-cpp\Build\Interactivity.UWP.Cpp\Interactivity.UWP.Cpp.vcxproj /p:Configuration=Release /p:Platform=Win32
 
 xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\Durango\casablanca140.xbox\*.* %~dp0\Lib\XboxOne
 xcopy /Y /I %~dp0\interactive-cpp\Binaries\Release\Durango\Interactivity.Xbox.Cpp\*.* %~dp0\Lib\XboxOne
